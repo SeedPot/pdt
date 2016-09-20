fc4067312d4c2b32c343cf7986d863637bce5763
Administrador <Administrador@Mac-mini-de-Administrador.local>
2016-09-20 17:33:44 -0300

    Header

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,21 @@
 const gulp = require('gulp'), sass = require('gulp-sass'), browser = require('browser-sync').create();
 
 gulp.task('sass', function(){
-    return gulp.src('./sass/*.scss')
+    return gulp.src('./src/sass/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('./src/sass/*.scss', ['sass']);
+    gulp.watch('./public/*.html').on('change', browser.reload);
+    gulp.watch('./public/css/*.css').on('change', browser.reload);
 });
+
+gulp.task('sync', function(){
+    browser.init({
+        server: './public'
+    });
+});
+
+gulp.task('default', ['sync', 'watch']);
