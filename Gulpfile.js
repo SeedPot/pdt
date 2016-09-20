3cccb79c83e0eb15ab27b3edb148449232f76908
Administrador <Administrador@Mac-mini-de-Administrador.local>
2016-09-20 18:39:25 -0300

    Estructura de los archivos scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp'), sass = require('gulp-sass'), browser = require('browser-sync').create();
 
 gulp.task('sass', function(){
-    return gulp.src('./src/sass/*.scss')
+    return gulp.src('./src/sass/styles.scss')
     .pipe(sass())
     .pipe(gulp.dest('./public/css'));
 });
