8120d62d1e7e849cfbd7f1a2ccc0d3c8fad17d7a
Hallein <piratemani@gmail.com>
2016-10-14 20:46:00 -0300

    Agregada seccion Programa

---
+++ public/programa.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Programa</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/flexboxgrid.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/programa.css">
+</head>
+<body>  
+
+<header>
+    <div id="header" style="background-color: rgba(0,0,0,.4);">
+        <?php include_once 'header.php'; ?>
+    </div>
+</header>
+
+<main class="pro-container">
+
+    <div class="row center-xs">
+        <div class="col-xs-10 col-lg-12 pro-title">
+            <h1>Programa de Difusión Tecnológica</h1>
+        </div>        
+        <div class="col-xs-10 col-sm-10 col-md-6">
+            <hr class="pro-hr-title azul">
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-lg-6 last-xs first-lg">
+            <img class="pro-img" src="images/grupo2_opt.jpg" alt="">
+        </div>
+
+        <div class="col-xs-12 col-lg-6">
+            <p class="pro-text">
+            Dada coyuntura económica con una importante caída de la actividad minera y otros sectores, CORFOproyecta que un sector en desarrollo y crecimiento al cual pueden incorporarse empresas de la región connuevas líneas de negocios sería el de la energía renovable solar, específicamente prestar servicios ogenerar soluciones que operan con energía solar fotovoltaicas y termo solar. 
+            La existencia de una red de proveedores regionales vinculada a la minería, conformada por empresas de construcción, metalmecánicas, de mantención y de servicios eléctricos que vienen realizado actividades prospectivas en el ámbito de las energías renovables o presentan interés en el tema, unido a las oportunidades que ofrece la industria de las energías renovables (Chile importa el 60% de su energía primaria según Balance Nacional de Energía BNE 2012, los hidrocarburos como carbón, petróleo y gas, que se proyectan con escasez y alta competencia por su uso, la alta demanda y los altos costos de la electricidad que se estima en un 34% más para la próxima década, una agenda de desarrollo por parte del ministerio de energía y normativas que proyectan contar con un 20% de participación de ERNC al año 2025), de acuerdo a informes de Greenpeace, la energía solar podrá suministrar electricidad a dos tercios de la población mundial en 2030 y según un estudio publicado en 2007 por el Consejo Mundial de Energía, para el año 2100 el 70 % de la energía consumida será de origen solar.
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-lg-12">
+            <p class="pro-text">
+                <span class="pro-text-bold">En el caso de Chile, la energía solar se puede transformar en una de las más competitivas debido a las economías de escala y avances tecnológicos. Ello, sumado a la radiación más alta del mundo que posee el Norte de Chile, permite proyectar a la energía solar como de las fuentes de energía de las más competitivas en nuestro país.</span> &quot;A modo de ejemplo, en Chile se puede obtener el doble de la producción de energía que en Alemania (país con la mayor capacidad solar instalada en el mundo) y 45% más que en el sur de España y tenemos la ventaja de que la demanda (industria minera) está en el lugar donde se encuentra el recurso solar (desierto), lo cual reduce la inversión asociada a la transmisión.&quot;  
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-lg-6 first-lg last-xs">
+            <p class="pro-text pro-text-bold">
+                En este contexto, CORFO ha aprobado un programa de difusión tecnológica denominado “Tecnología e innovación para el desarrollo de la industria auxiliar proveedora de proyectos de generación y soluciones que operan con energía solar “ que será desarrollado por la empresa consultora Support Consultoría y que incluye actividades de diagnóstico, capacitación con expertos de Chile y Alemania en temas como Instalación de Sistemas Fotovoltaicos y Conexión a la Red, Instalación de Sistemas Solares Térmicos, Diseño y fabricación de componentes para la industria solar, Sistemas para el almacenamiento de energía renovable, Vigilancia Tecnológica, Estrategias y Herramientas para Innovar, servicios de mantención y desarrollo de negocios. Además se realizarán seminarios, visita a proyectos en operación, acceso a redes, laboratorio de proyectos, búsqueda de financiamiento y otros. 
+            </p>
+            <p class="pro-text pro-text-bold">
+                El programa cuenta de 40 cupos para empresas pequeñas medianas las cuales podrán destinar dos participantes por empresa, más detalles pueden ser obtenido con Alejandra Escalona Gerente de Support Consultores cuyas oficinas están ubicadas en Sotomayor 575 oficina 504 o al correo gerencia@supportconsultores.cl
+            </p>
+        </div>
+
+        <div class="col-xs-12 col-lg-6">
+            <img class="pro-img pro-no-padding" src="images/delegacion_opt.jpg" alt="">
+        </div>
+
+    </div>
+
+</main>
+
+<div class="megafooter">
+    <footer id="footer"><?php include_once 'footer.php' ?></footer>
+
+    <section class="pretzel no-margin row center-xs middle-xs">
+        <div class="col-xs-6">
+            <p>Pretzel Studio<sup>&reg;</sup> 2016</p>
+        </div>
+    </section>
+</div>  
+
+</body>
+</html>
